0c3052d97860374c2f1bbc4199382cac2bbdd2d0
mfyang <mfyang@gmail.com>
2013-02-25 11:00:08 -0800

    add script for deployment

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#deploy all dotfiles 
+
+for file in $(ls -Ad $HOME/.dotfiles/.[a-z]*); do  
+
+	if [ -f $file ]; then
+		target=$HOME/$(basename $file)
+		echo "aliasing file $file to $target" 
+		mv $target $target.bak
+		ln -sf $file $target
+	fi
+
+done
